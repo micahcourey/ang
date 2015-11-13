f70469cd9bd8b97387c23b54c432b4e99ab0529b
micahcourey <micahjcourey@gmail.com>
2015-11-13 14:45:41 -0800

    Add angular-listing.tpl.php

---
+++ angular-listing.tpl.php
@@ -0,0 +1,13 @@
+<div ng-app="nodeListing">
+  <div ng-controller="ListController">
+    <h3>Filter</h3>
+    <input ng-model="search" ng-change="doSearch()">
+    <ul>
+      <li ng-repeat="node in nodes"><button ng-click="open(node.nid)">Open</button> {{ node.title }}</li>
+    </ul>
+    <script type="text/ng-template" id="loadedNodeTemplate">
+    <h3>{{ loadedNode.title }}</h3>
+    {{ loadedNode.body }}
+    </script>
+  </div>
+</div>
